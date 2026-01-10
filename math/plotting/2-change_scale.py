dc61154afcf945d5db80639775ff53da6d06e258
firuzaisgandarli <firuze.isgenderli41@gmail.com>
2026-01-10 20:16:02 +0000

    Add change_scale function to plot exponential decay of C-14 with logarithmic y-axis

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Module that plots exponential decay of C-14
+with logarithmic y-axis scaling.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    """
+    Plot exponential decay of C-14 with logarithmic y-axis.
+    """
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plot line
+    plt.plot(x, y, 'b-')
+
+    # Labels and title
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.title("Exponential Decay of C-14")
+
+    # Logarithmic scale for y-axis
+    plt.yscale("log")
+
+    # Set x-axis range
+    plt.xlim(0, 28650)
+
+    # Show plot
+    plt.show()
