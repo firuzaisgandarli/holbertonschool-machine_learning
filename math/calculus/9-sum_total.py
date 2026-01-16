120e4ff1261ecfd723d821c979a5aa286b062e8b
SÉfiruzaisgandarli <sfiruze.isgenderli41@gmail.com>
2026-01-16 11:51:13 +0000

    Add task 9: summation_i_squared function

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module that provides summation of squares from 1 to n
+"""
+
+def summation_i_squared(n):
+    """
+    Calculates the sum of squares from 1 to n.
+    Returns integer value if n is valid, otherwise None.
+    """
+    if not isinstance(n, int) or n < 1:
+        return None
+    return n * (n + 1) * (2 * n + 1) // 6
+
