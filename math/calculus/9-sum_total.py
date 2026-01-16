5a6221494c53ca5f0873c9a829d6e1c0d872c31c
SÉfiruzaisgandarli <sfiruze.isgenderli41@gmail.com>
2026-01-16 11:55:20 +0000

    Add task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,6 +3,7 @@
 Module that provides summation of squares from 1 to n
 """
 
+
 def summation_i_squared(n):
     """
     Calculates the sum of squares from 1 to n.
@@ -11,4 +12,3 @@ def summation_i_squared(n):
     if not isinstance(n, int) or n < 1:
         return None
     return n * (n + 1) * (2 * n + 1) // 6
-
