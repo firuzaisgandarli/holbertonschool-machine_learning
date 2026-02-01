cdc61af7a8f514a52bb9e003f976fdca66a52e0f
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-01 15:49:18 +0000

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Numpy matrix multiplication module"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication using numpy"""
+    return np.matmul(mat1, mat2)
