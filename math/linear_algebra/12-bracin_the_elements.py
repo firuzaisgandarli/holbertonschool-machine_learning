bbcabcc2681d51d1b4f3d4c622e59ec5681bcd13
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-01 15:46:35 +0000

    task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Numpy element-wise operations module"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Performs element-wise addition, subtraction,
+    multiplication, and division
+    """
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
