3289a50445d446c971c6f95b908dfa8afa880ffe
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-08 13:20:08 +0000

    added task 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -20,7 +20,9 @@ def determinant(matrix):
     """
     if not isinstance(matrix, list) or \
        any(not isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a list of lists")
+        raise TypeError(
+            "matrix must be a list of lists"
+        )
 
     if matrix == [[]]:
         return 1
