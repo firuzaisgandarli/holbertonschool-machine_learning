18429d60233a5f32d548577fd302cb15e49c49ba
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-14 08:54:09 +0000

    add task 2

--- math/probability/2-main.py
+++ math/probability/2-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import numpy as np
 Poisson = __import__('poisson').Poisson
 
-np.random.seed(0)
-data = np.random.poisson(5., 100).tolist()
+# Example dataset to estimate lambtha
+data = [2, 4, 6, 5, 7, 3, 5, 4, 6, 5]
 p1 = Poisson(data)
 print('F(9):', p1.cdf(9))
 
+# Using a fixed lambtha
 p2 = Poisson(lambtha=5)
 print('F(9):', p2.cdf(9))
