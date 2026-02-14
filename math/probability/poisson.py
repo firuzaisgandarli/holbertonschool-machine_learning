7e21595c489d0d3493c4efe0c9cd97e6d3344dc0
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-14 09:00:56 +0000

    task 1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -26,22 +26,10 @@ class Poisson:
             return 0
 
         e = 2.7182818285
+
         factorial = 1
         for i in range(1, k + 1):
             factorial *= i
 
         return ((self.lambtha ** k) *
                 (e ** (-self.lambtha))) / factorial
-
-    def cdf(self, k):
-        """Calculates the value of the CDF for a given number of successes"""
-        if not isinstance(k, int):
-            k = int(k)
-        if k < 0:
-            return 0
-
-        cdf = 0
-        for i in range(0, k + 1):
-            cdf += self.pmf(i)
-
-        return cdf
