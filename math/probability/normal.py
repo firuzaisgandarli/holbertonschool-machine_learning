419a929c2ca395f7e6fa29f44bb1e8708c4bc639
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-14 09:08:02 +0000

    task 7

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -28,3 +28,11 @@ class Normal:
 
             self.mean = float(mean_value)
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return z * self.stddev + self.mean
