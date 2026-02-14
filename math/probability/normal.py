6e6b011f84be034e670bee3b4de96b70e125ecf7
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-14 18:06:28 +0000

    added1task 9

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -3,10 +3,13 @@
 
 
 class Normal:
-    """Class that represents a normal distribution"""
+    """Represents a normal distribution"""
+
+    e = 2.7182818285
+    pi = 3.1415926536
 
     def __init__(self, data=None, mean=0., stddev=1.):
-        """Initialize Normal distribution"""
+        """Initialize the Normal distribution"""
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
@@ -30,33 +33,29 @@ class Normal:
         return z * self.stddev + self.mean
 
     def pdf(self, x):
-        """Calculates the PDF for a given x-value"""
-        pi = 3.1415926536
-        e = 2.7182818285
-        coef = 1 / (self.stddev * (2 * pi) ** 0.5)
+        """Calculates the value of the PDF for a given x-value"""
+        coeff = 1 / (self.stddev * (2 * self.pi) ** 0.5)
         exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
-        return coef * (e ** exponent)
+        return coeff * (self.e ** exponent)
 
     def cdf(self, x):
-        """Calculates the CDF for a given x-value"""
-        pi = 3.1415926536
-        e = 2.7182818285
-
-        # z-score normalized for erf
+        """Calculates the value of the CDF for a given x-value"""
         z = (x - self.mean) / (self.stddev * (2 ** 0.5))
 
-        # Abramowitz & Stegun approximation
         t = 1 / (1 + 0.3275911 * abs(z))
+
         a1 = 0.254829592
         a2 = -0.284496736
         a3 = 1.421413741
         a4 = -1.453152027
         a5 = 1.061405429
 
-        erf_approx = 1 - (((((a5 * t + a4) * t + a3) * t + a2) *
-                          t + a1) * t * (e ** (-z * z)))
+        erf = 1 - (
+            (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1)
+            * t * (self.e ** (-z * z))
+        )
 
         if z < 0:
-            erf_approx = -erf_approx
+            erf = -erf
 
-        return 0.5 * (1 + erf_approx)
+        return 0.5 * (1 + erf)
