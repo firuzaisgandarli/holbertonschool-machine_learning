c0f49ca0ce92b64b988815e26a220ba08e850f9d
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-14 18:43:42 +0000

    Add task 9

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,61 +1,69 @@
 #!/usr/bin/env python3
-"""Normal distribution module"""
+"""
+Normal distribution module
+"""
 
 
 class Normal:
-    """Represents a normal distribution"""
+    """
+    Normal distribution class
+    """
 
     e = 2.7182818285
     pi = 3.1415926536
 
     def __init__(self, data=None, mean=0., stddev=1.):
-        """Initialize the Normal distribution"""
+        """
+        Initialize a Normal distribution
+        data: list of data to estimate mean and stddev
+        mean: given mean if data is None
+        stddev: given stddev if data is None
+        """
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
             self.mean = float(mean)
             self.stddev = float(stddev)
         else:
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.mean = sum(data) / len(data)
-            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
-            self.stddev = variance ** 0.5
+            self.mean = float(sum(data) / len(data))
+            self.stddev = float(
+                (sum((x - self.mean) ** 2 for x in data) / len(data)) ** 0.5
+            )
 
     def z_score(self, x):
-        """Calculates the z-score of a given x-value"""
+        """
+        Calculates the z-score of a given x-value
+        """
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
-        """Calculates the x-value of a given z-score"""
+        """
+        Calculates the x-value of a given z-score
+        """
         return z * self.stddev + self.mean
 
     def pdf(self, x):
-        """Calculates the value of the PDF for a given x-value"""
-        coeff = 1 / (self.stddev * (2 * self.pi) ** 0.5)
-        exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
-        return coeff * (self.e ** exponent)
+        """
+        Calculates the value of the PDF for a given x-value
+        """
+        exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
+        coefficient = 1 / (self.stddev * (2 * Normal.pi) ** 0.5)
+        return coefficient * (Normal.e ** exponent)
 
     def cdf(self, x):
-        """Calculates the value of the CDF for a given x-value"""
+        """
+        Calculates the value of the CDF for a given x-value
+        """
+        # z = (x - mean) / (stddev * sqrt(2))
         z = (x - self.mean) / (self.stddev * (2 ** 0.5))
 
-        t = 1 / (1 + 0.3275911 * abs(z))
-
-        a1 = 0.254829592
-        a2 = -0.284496736
-        a3 = 1.421413741
-        a4 = -1.453152027
-        a5 = 1.061405429
-
-        erf = 1 - (
-            (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1)
-            * t * (self.e ** (-z * z))
+        # approximate erf(z) using a 5-term Maclaurin series
+        erf = (2 / Normal.pi ** 0.5) * (
+            z - (z ** 3) / 3 + (z ** 5) / 10 - (z ** 7) / 42 + (z ** 9) / 216
         )
-
-        if z < 0:
-            erf = -erf
 
         return 0.5 * (1 + erf)
