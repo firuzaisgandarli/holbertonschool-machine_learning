510d700cec70fbb708d9091523cc694ddce33b8c
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-02-14 09:09:53 +0000

    task 8

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -36,3 +36,11 @@ class Normal:
     def x_value(self, z):
         """Calculates the x-value of a given z-score"""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
+        pi = 3.1415926536
+        e = 2.7182818285
+        coefficient = 1 / (self.stddev * ((2 * pi) ** 0.5))
+        exponent = e ** (-0.5 * (((x - self.mean) / self.stddev) ** 2))
+        return coefficient * exponent
