2b326b88fa674b5c2c9c2af2862f80ef7b3c1124
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-03-08 15:58:07 +0000

    Fix DeepNeuralNetwork He initialization and add docstrings

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
+"""
+Module defines a DeepNeuralNetwork for binary classification
+"""
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network performing binary classification."""
+    """
+    Class that defines a deep neural network
+    """
 
     def __init__(self, nx, layers):
         """
-        Initialize the deep neural network.
-
-        Parameters
-        ----------
-        nx : int
-            Number of input features.
-        layers : list of int
-            List representing the number of nodes in each layer.
+        Class constructor
+        Args:
+            nx: number of input features
+            layers: list representing the number of nodes in each layer
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -22,19 +23,23 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(x, int) and x > 0 for x in layers):
-            raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for l in range(self.L):
-            layer_size = layers[l]
-            prev_size = nx if l == 0 else layers[l - 1]
-            # He et al. initialization
-            self.weights["W{}".format(l + 1)] = (
-                np.random.randn(layer_size, prev_size) * np.sqrt(2 / prev_size)
-            )
-            self.weights["b{}".format(l + 1)] = np.zeros((layer_size, 1))
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
 
+            # Layer indexing starts at 1
+            curr_layer = i + 1
+            # Previous size is nx for the first layer, else layers[i-1]
+            prev_size = nx if i == 0 else layers[i - 1]
+
+            # He et al. initialization: randn * sqrt(2 / size_of_previous_layer)
+            self.weights["W" + str(curr_layer)] = (
+                np.random.randn(layers[i], prev_size) * np.sqrt(2 / prev_size)
+            )
+            # Biases initialized to zeros
+            self.weights["b" + str(curr_layer)] = np.zeros((layers[i], 1))
