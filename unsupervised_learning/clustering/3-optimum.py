5dcb99404ad266557b0c881ecbef18ac7bb40e29
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-05-24 18:32:37 +0000

    task 3

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,28 +1,38 @@
+#!/usr/bin/env python3
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 variance = __import__('2-variance').variance
+
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
     Finds optimal k using variance differences.
     """
+
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
+
     if kmax is None:
         kmax = kmin + 1
+
+    if not isinstance(kmin, int) or not isinstance(kmax, int):
+        return None, None
 
     if kmin <= 0 or kmax <= 0 or kmax < kmin:
         return None, None
 
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+
     results = []
-    vars_ = []
+    vars_list = []
 
     for k in range(kmin, kmax + 1):
         C, clss = kmeans(X, k, iterations)
         results.append((C, clss))
-        vars_.append(variance(X, C))
+        vars_list.append(variance(X, C))
 
-    vars_ = np.array(vars_)
-    d_vars = vars_[0] - vars_
+    vars_arr = np.array(vars_list)
+    d_vars = vars_arr[0] - vars_arr
 
     return results, d_vars
