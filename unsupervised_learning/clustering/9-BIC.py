fa9c7b4f8d16af0a28fb7634243d3dcf4576f523
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-05-24 19:25:17 +0000

    task 9

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -5,7 +5,7 @@ expectation_maximization = __import__('8-EM').expectation_maximization
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
-    Finds best number of clusters using BIC for a GMM.
+    Computes best number of clusters for a GMM using BIC.
     """
 
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
@@ -19,36 +19,33 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if kmin <= 0 or kmax <= 0 or kmax < kmin:
         return None, None, None, None
 
-    ks = list(range(kmin, kmax + 1))
+    ks = range(kmin, kmax + 1)
 
     results = []
-    l_vals = []
-    b_vals = []
+    log_likelihoods = []
+    bics = []
 
     best_k = None
     best_result = None
     best_bic = np.inf
 
     for k in ks:
-        pi, m, S, g, log_likelihood = expectation_maximization(
+        pi, m, S, g, l = expectation_maximization(
             X, k, iterations=iterations, tol=tol, verbose=verbose
         )
 
-        # number of parameters:
-        # pi: k-1 (since sum to 1)
-        # m: k*d
-        # S: k*(d(d+1)/2)
+        # number of parameters in full GMM
         p = (k - 1) + (k * d) + (k * d * (d + 1)) // 2
 
-        bic = p * np.log(n) - 2 * log_likelihood
+        bic = p * np.log(n) - 2 * l
 
         results.append((pi, m, S))
-        l_vals.append(log_likelihood)
-        b_vals.append(bic)
+        log_likelihoods.append(l)
+        bics.append(bic)
 
         if bic < best_bic:
             best_bic = bic
             best_k = k
             best_result = (pi, m, S)
 
-    return best_k, best_result, np.array(l_vals), np.array(b_vals)
+    return best_k, best_result, np.array(log_likelihoods), np.array(bics)
