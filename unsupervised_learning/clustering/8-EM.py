0829f38ff48d2de3f9ee99fd6791eabca62865a1
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-05-24 19:32:26 +0000

    task 8

---
+++ unsupervised_learning/clustering/8-EM.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import numpy as np
+
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
+
+
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
+    """
+    Performs EM algorithm for a GMM.
+    """
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None, None
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None, None
+
+    pi, m, S = initialize(X, k)
+    if pi is None:
+        return None, None, None, None, None
+
+    prev_l = None
+
+    for i in range(iterations + 1):
+
+        g, l = expectation(X, pi, m, S)
+        if g is None:
+            return None, None, None, None, None
+
+        if maximization(X, g) is None:
+            return None, None, None, None, None
+
+        pi, m, S = maximization(X, g)
+
+        if verbose and (i % 10 == 0 or i == iterations):
+            print("Log Likelihood after {} iterations: {:.5f}".format(i, l))
+
+        if prev_l is not None and abs(l - prev_l) <= tol:
+            break
+
+        prev_l = l
+
+    return pi, m, S, g, l
