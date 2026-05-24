f13f6ba8afb900cd9561a76e2b3d575774184c6d
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2026-05-24 19:18:13 +0000

    task 11

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,45 +1,29 @@
 #!/usr/bin/env python3
 import numpy as np
-from sklearn.mixture import GaussianMixture
+import sklearn.mixture
 
 
 def gmm(X, k):
     """
     Performs Gaussian Mixture Model clustering on a dataset.
-
-    Parameters:
-    - X: numpy.ndarray of shape (n, d), dataset
-    - k: number of clusters
-
-    Returns:
-    - pi: cluster priors (k,)
-    - m: cluster means (k, d)
-    - S: covariance matrices (k, d, d)
-    - clss: cluster assignments (n,)
-    - bic: Bayesian Information Criterion score
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
     if not isinstance(k, int) or k <= 0:
         return None, None, None, None, None
 
-    # Fit Gaussian Mixture Model
-    gmm_model = GaussianMixture(n_components=k, covariance_type='full')
-    gmm_model.fit(X)
+    # Initialize and fit model
+    model = sklearn.mixture.GaussianMixture(
+        n_components=k,
+        covariance_type='full'
+    )
+    model.fit(X)
 
-    # Priors (weights)
-    pi = gmm_model.weights_
-
-    # Means
-    m = gmm_model.means_
-
-    # Covariances
-    S = gmm_model.covariances_
-
-    # Cluster assignments
-    clss = gmm_model.predict(X)
-
-    # BIC score
-    bic = gmm_model.bic(X)
+    # Extract parameters
+    pi = model.weights_
+    m = model.means_
+    S = model.covariances_
+    clss = model.predict(X)
+    bic = model.bic(X)
 
     return pi, m, S, clss, bic
